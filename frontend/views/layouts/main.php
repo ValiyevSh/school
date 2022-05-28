9c6ff6f6da69eccd3d4ed9448defbd4c203f9c32
Shuhratbek <102726853+ValiyevSh@users.noreply.github.com>
2022-05-28 05:55:07 +0500

    change
    change

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -60,18 +60,8 @@ AppAsset::register($this);
             </div>
             <div class="col-lg-3">
                 <div class="header_info">
-<!--                    <div class="search"><a href="#"><i class="fas fa-search"></i></a></div>-->
-<!--                    <div class="loginwrp"><a href="login.html">Login/Register</a></div>-->
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown button
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
+                    <div class="search"><a href="#"><i class="fas fa-search"></i></a></div>
+                    <div class="loginwrp"><a href="login.html">Login/Register</a></div>
                 </div>
             </div>
         </div>
